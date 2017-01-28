b1832e19dfca1322aea84f536ff306d95cb051a6
dh1tw <tobias@dh1tw.de>
2017-01-28 17:56:54 +0100

    improved travis-ci and added deployment of releases

--- Makefile
+++ Makefile
@@ -15,6 +15,14 @@ build:
 	go build -v -o ${OUT} -ldflags="-X github.com/dh1tw/remoteAudio/cmd.commitHash=${COMMITID} \
 		-X github.com/dh1tw/remoteAudio/cmd.version=${VERSION}"
 
+# strip off dwraf table - used for travis CI
+dist: 
+	protoc --proto_path=./icd --gofast_out=./sb_audio ./icd/audio.proto
+	cd webserver; \
+	rice embed-go 
+	go build -v -o ${OUT} -ldflags="-w -X github.com/dh1tw/remoteAudio/cmd.commitHash=${COMMITID} \
+		-X github.com/dh1tw/remoteAudio/cmd.version=${VERSION}"
+
 # test:
 # 	@go test -short ${PKG_LIST}
 
--- install-protobuf.sh
+++ install-protobuf.sh
@@ -2,9 +2,12 @@
 set -e
 # check to see if protobuf folder is empty
 if [ ! -d "$HOME/protobuf/lib" ]; then
-  wget https://https://github.com/google/protobuf/archive/v3.1.0.tar.gz
-  tar -xzvf v3.1.0.tar.gz
-  cd protobuf-3.1.0 && ./autogen.sh && ./configure --prefix=$HOME/protobuf && make && make install
+  CURRDIR= pwd #cache current directory
+  wget -P $HOME https://github.com/google/protobuf/archive/v3.1.0.tar.gz
+  tar -xzvf $HOME/v3.1.0.tar.gz -C $HOME
+  cd $HOME/protobuf-3.1.0 && ./autogen.sh && ./configure --prefix=$HOME/protobuf && make && make install
+  chmod +x $HOME/protobuf/bin/protoc
+  cd $CURDIR #get back to initial directory
 else
   echo "Using cached directory."
 fi
