1e4a3c5f8d787b209121879b0cf4bdd0c1ce5247
Tobias Wellnitz, DH1TW <tobias@dh1tw.de>
2018-03-28 02:20:36 +0200

    added sampling rate converter

--- audio/pbWriter/pbWriter.go
+++ audio/pbWriter/pbWriter.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"sync"
 
+	"github.com/dh1tw/gosamplerate"
 	"github.com/dh1tw/remoteAudio/audio"
 	"github.com/dh1tw/remoteAudio/audiocodec/opus"
 	"github.com/gogo/protobuf/proto"
@@ -13,14 +14,29 @@ import (
 	sbAudio "github.com/dh1tw/remoteAudio/sb_audio"
 )
 
+// PbWriter implements the audio.Sink interface. It is used to encode
+// audio.Msg with a selected audiocodec into Protocol buffers. This
+// Sink is typically used when audio frames have to be send to the network.
 type PbWriter struct {
 	sync.RWMutex
 	options Options
 	enabled bool
 	buffer  []byte
 	cb      func([]byte)
+	src     src
 }
 
+// src contains a samplerate converter and its needed variables
+type src struct {
+	gosamplerate.Src
+	samplerate float64
+	ratio      float64
+}
+
+// NewPbWriter is the constructor for a ProtoBufWriter. It has to be given
+// a Callback which will be called when an audio.Msg has been encoded into a
+// protobuf byte slice. Additional functional options can be passed in (e.g. the
+// audio codec to be used).
 func NewPbWriter(cb func([]byte), opts ...Option) (*PbWriter, error) {
 
 	pbw := &PbWriter{
@@ -48,30 +64,63 @@ func NewPbWriter(cb func([]byte), opts ...Option) (*PbWriter, error) {
 		pbw.options.Encoder = enc
 	}
 
+	// setup a samplerate converter
+	srConv, err := gosamplerate.New(gosamplerate.SRC_SINC_FASTEST,
+		pbw.options.Channels, 65536)
+	if err != nil {
+		return nil, fmt.Errorf("PbWriter samplerate converter: %v", err)
+	}
+	pbw.src = src{
+		Src:        srConv,
+		samplerate: pbw.options.Samplerate,
+		ratio:      1,
+	}
+
 	return pbw, nil
 }
 
+// Start starts this audio sink.
 func (pbw *PbWriter) Start() error {
+	pbw.Lock()
+	defer pbw.Unlock()
+	pbw.enabled = true
 	return nil
 }
 
+// Stop disables this audio sink.
 func (pbw *PbWriter) Stop() error {
+	pbw.Lock()
+	defer pbw.Unlock()
+	pbw.enabled = false
 	return nil
 }
 
+// Close is not implemented
 func (pbw *PbWriter) Close() error {
 	return nil
 }
 
+// SetVolume is not implemented for this Sink
 func (pbw *PbWriter) SetVolume(vol float32) {
 
 }
 
+// Volume is not implemented for this Sink
 func (pbw *PbWriter) Volume() float32 {
 	return 1
 }
 
+// Write is called to encode audio.Msg with a specified audio codec into
+// protobufs. The Token is not used. On success the protobuf ([]byte) will
+// be returned in a callback.
 func (pbw *PbWriter) Write(audioMsg audio.Msg, token audio.Token) error {
+
+	pbw.Lock()
+	defer pbw.Unlock()
+
+	if !pbw.enabled {
+		return nil
+	}
 
 	if pbw.cb == nil {
 		return nil
@@ -82,20 +131,46 @@ func (pbw *PbWriter) Write(audioMsg audio.Msg, token audio.Token) error {
 		return errors.New("no encoder set")
 	}
 
-	// check if channels, Frames number, Samplerate correspond with codec
+	var aData []float32
 
+	// if necessary adjust the amount of audio channels
+	if audioMsg.Channels != pbw.options.Channels {
+		aData = audio.AdjustChannels(audioMsg.Channels,
+			pbw.options.Channels, audioMsg.Data)
+	} else {
+		aData = audioMsg.Data
+	}
+
+	channels := sbAudio.Channels_unknown
+	switch audioMsg.Channels {
+	case 1:
+		channels = sbAudio.Channels_mono
+	case 2:
+		channels = sbAudio.Channels_stereo
+	}
+
+	// The resampling and encoding can be quite expensive (e.g. with opus). Therefore it is
+	// launched in a separate go routine.
 	go func() {
-		num, err := pbw.options.Encoder.Encode(audioMsg.Data, pbw.buffer)
-		if err != nil {
-			fmt.Println(err)
+
+		var err error
+
+		if audioMsg.Samplerate != pbw.options.Samplerate {
+			if pbw.src.samplerate != audioMsg.Samplerate {
+				pbw.src.Reset()
+				pbw.src.samplerate = audioMsg.Samplerate
+				pbw.src.ratio = pbw.options.Samplerate / audioMsg.Samplerate
+			}
+			aData, err = pbw.src.Process(aData, pbw.src.ratio, false)
+			if err != nil {
+				log.Println(err)
+				return
+			}
 		}
 
-		channels := sbAudio.Channels_unknown
-		switch audioMsg.Channels {
-		case 1:
-			channels = sbAudio.Channels_mono
-		case 2:
-			channels = sbAudio.Channels_stereo
+		num, err := pbw.options.Encoder.Encode(aData, pbw.buffer)
+		if err != nil {
+			fmt.Println(err)
 		}
 
 		msg := sbAudio.Frame{
@@ -120,6 +195,7 @@ func (pbw *PbWriter) Write(audioMsg audio.Msg, token audio.Token) error {
 	return nil
 }
 
+// Flush is not implemented
 func (pbw *PbWriter) Flush() {
 
 }
