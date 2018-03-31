3b852cec64fd88421dd45dc60f1f9aec96ca3cd3
Tobias Wellnitz, DH1TW <tobias@dh1tw.de>
2018-03-31 16:47:43 +0200

    updated protobuf to 3.5.1

--- ci/install-protobuf.sh
+++ ci/install-protobuf.sh
@@ -3,9 +3,9 @@ set -e
 # check to see if protobuf folder is empty
 if [ ! -d "$HOME/protobuf/lib" ]; then
   CURRDIR= pwd #cache current directory
-  wget -P $HOME https://github.com/google/protobuf/archive/v3.1.0.tar.gz
-  tar -xzvf $HOME/v3.1.0.tar.gz -C $HOME
-  cd $HOME/protobuf-3.1.0 && ./autogen.sh && ./configure --prefix=$HOME/protobuf && make && make install
+  wget -P $HOME https://github.com/google/protobuf/archive/v3.5.1.tar.gz
+  tar -xzvf $HOME/v3.5.1.tar.gz -C $HOME
+  cd $HOME/protobuf-3.5.1 && ./autogen.sh && ./configure --prefix=$HOME/protobuf && make && make install
   chmod +x $HOME/protobuf/bin/protoc
   cd $CURDIR #get back to initial directory
   export PATH="$PATH:$HOME/protobuf/bin"
