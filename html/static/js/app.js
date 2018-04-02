04b63683cfebe9c427d4da009539e6d9b46a8a5b
Tobias Wellnitz, DH1TW <tobias@dh1tw.de>
2018-04-02 15:47:47 +0200

    minor tweaks

--- html/static/js/app.js
+++ html/static/js/app.js
@@ -2,14 +2,12 @@ Vue.config.devtools = true
 
 $.material.init();
 
-
 var vm = new Vue({
     el: '#app',
 
     data: {
         ws: null, // Our websocket
         txOn: false,
-        txUser: null,
         connectionState: false,
         blockRxVolumeUpdate: false,
         blockTxVolumeUpdate: false,
@@ -21,86 +19,82 @@ var vm = new Vue({
         'audioservers': AudioServers,
     },
     mounted: function () {
-        var self = this;
-        this.ws = new ReconnectingWebSocket('ws://' + window.location.host + '/ws');
-        this.ws.addEventListener('message', function (e) {
-            var msg = JSON.parse(e.data);
-            console.log(msg);
+        this.openWebsocket();
+    },
+    methods: {
+        openWebsocket: function () {
+            var self = this;
+            this.ws = new ReconnectingWebSocket('ws://' + window.location.host + '/ws');
+            this.ws.addEventListener('message', this.processWsMsg);
+            this.ws.addEventListener('open', function () {
+                self.wsConnected = true
+                setTimeout(function () {
+                    self.hideWsConnectionMsg = true;
+                }, 1500)
+            });
+            this.ws.addEventListener('close', function () {
+                self.wsConnected = false
+                self.hideWsConnectionMsg = false;
+            });
+        },
+        processWsMsg: function (rawMsg) {
+            var msg = JSON.parse(rawMsg.data);
+            // console.log(msg);
 
             if (msg.tx_on !== null) {
-                self.txOn = msg.tx_on;
+                this.txOn = msg.tx_on;
             }
 
             if (msg.rx_volume !== null) {
                 // only allow updates if we are not modifying the handle
-                if (!self.blockRxVolumeUpdate) {
-                    rxVolumeSlider.noUiSlider.set(msg.rx_volume);
+                if (!this.blockRxVolumeUpdate) {
+                    if (rxVolumeSlider.noUiSlider.get() != msg.rx_volume) {
+                        rxVolumeSlider.noUiSlider.set(msg.rx_volume);
+                    }
                 }
             }
 
             if (msg.tx_volume !== null) {
-                if (!self.blockTxVolumeUpdate) {
-                    txVolumeSlider.noUiSlider.set(msg.tx_volume);
+                if (!this.blockTxVolumeUpdate) {
+                    if (txVolumeSlider.noUiSlider.get() != msg.tx_volume) {
+                        txVolumeSlider.noUiSlider.set(msg.tx_volume);
+                    }
                 }
             }
 
             if (msg.connected !== null) {
-                self.connectionState = msg.connected;
+                this.connectionState = msg.connected;
             }
 
             if (msg.audio_servers !== null) {
-                self.$set(self, "audioServers", msg.audio_servers)
-                // if (msg.audio_servers.length == 0) {
-                //     self.$set(self, "audioServers", {})
-                //     console.log(self.audioServers)
-                // } else {
-                //     for (var i = 0; i < msg.audio_servers.length; i++) {
-                //         this.$set(this.audioServers, as.name, as);
-                //     }
-                // }
+                this.updateAudioServers(msg.audio_servers)
             }
 
             if (msg.selected_server !== null) {
-                if (msg.selected_server !== this.selectedAudioServer) {
-                    self.selectServer(msg.selected_server);
-                }
+                this.selectServer(msg.selected_server);
+            }
+        },
+        selectServer: function (asName) {
+
+            if (asName == "") {
+                return
+            }
+            if (!(asName in this.audioServers)) {
+                return
             }
 
-            // if (msg.latency !== null) {
-            //     if (latencyChart.data.datasets[0].data.length >= 20) {
-            //         latencyChart.data.datasets[0].data.shift();
-            //     }
-            //     if (msg.ping > 500) {
-            //         latencyChart.data.datasets[0].data.push(500); // truncate
-            //     } else {
-            //         latencyChart.data.datasets[0].data.push(msg.latency);
-            //     }
-            //     latencyChart.update(0.1);
-            // }
-        });
-        this.ws.addEventListener('open', function () {
-            self.wsConnected = true
-            setTimeout(function () {
-                self.hideWsConnectionMsg = true;
-            }, 1500)
-        });
-        this.ws.addEventListener('close', function () {
-            self.wsConnected = false
-            self.hideWsConnectionMsg = false;
-        });
+            self = this;
+            Object.keys(this.audioServers).forEach(function(asName){
+                self.$set(self.audioServers[asName], "selected", false);
+            }),
 
-    },
-    methods: {
-        openWebsocket: function () {
-
+            this.$set(this.audioServers[asName], "selected", true);
         },
         setAudioServer: function (audioServerName) {
             this.$http.put("/api/server/" + audioServerName + "/active",
                 JSON.stringify({
                     active: true,
                 }));
-
-            console.log("selected: " + audioServerName);
         },
         setRxState: function (audioServerName, rxState) {
             console.log("set " + audioServerName + " to " + rxState)
@@ -109,23 +103,7 @@ var vm = new Vue({
                     on: rxState,
                 }));
         },
-        selectServer: function (asName) {
-            if (asName == "") {
-                console.log("empty")
-                return
-            }
-            this.$set(this.audioServers[asName], "selected", true)
-            console.log("selected:", this.audioServers[asName].selected)
-        },
-
-        // sendRxOn: function () {
-        //     this.$http.put("/api/rx/state",
-        //         JSON.stringify({
-        //             on: !this.rxOn,
-        //         }));
-        // },
         sendTxOn: function () {
-            // if (this.serverAudioOn) {
             this.$http.put("/api/tx/state",
                 JSON.stringify({
                     on: !this.txOn,
@@ -143,6 +121,39 @@ var vm = new Vue({
                     volume: Math.round(value)
                 }));
         },
+        updateAudioServers: function (aServers) {
+
+            // copy this into the variable self, as this will change
+            // within the forEach loops
+            self = this;
+
+            // iterate over audioServer objects and remove the ones which
+            // are not included anymore
+            Object.keys(this.audioServers).forEach(function (asName) {
+                if (!(asName in aServers)) {
+                    self.$delete(self.audioServers, asName)
+                }
+            })
+
+            // iterate over the received audioServer objects. Create the object
+            // or update the fields of the stored objects if necessary
+            Object.keys(aServers).forEach(function (asName) {
+                // if audioServer doesn't exist, add it
+                if (!(asName in self.audioServers)) {
+                    self.$set(self.audioServers, asName, aServers[asName])
+                } else { //just update the fields
+                    if (self.audioServers[asName].rx_on != aServers[asName].rx_on) {
+                        self.audioServers[asName].rx_on = aServers[asName].rx_on
+                    }
+                    if (self.audioServers[asName].tx_user != aServers[asName].tx_user) {
+                        self.audioServers[asName].tx_user = aServers[asName].tx_user
+                    }
+                    if (self.audioServers[asName].latency != aServers[asName].latency) {
+                        self.audioServers[asName].latency = aServers[asName].latency
+                    }
+                }
+            })
+        }
     },
 });
 
@@ -190,6 +201,9 @@ $(document).ready(function () {
             vm.sendRxVolume(Number(values[handle]));
         }
     });
+    rxVolumeSlider.noUiSlider.on('change', function (values, handle) {
+        vm.sendRxVolume(Number(values[handle]));
+    });
     //unblock the Volume slider updates through websocket
     rxVolumeSlider.noUiSlider.on('end', function (values, handle) {
         vm.blockRxVolumeUpdate = false;
@@ -207,69 +221,11 @@ $(document).ready(function () {
             vm.sendTxVolume(Number(values[handle]));
         }
     });
+    txVolumeSlider.noUiSlider.on('change', function (values, handle) {
+        vm.sendTxVolume(Number(values[handle]));
+    });
     //unblock the Volume slider updates through websocket
     txVolumeSlider.noUiSlider.on('end', function (values, handle) {
         vm.blockTxVolumeUpdate = false;
     });
 });
-
-
-
-
-// var data = {
-//     labels: ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
-//     datasets: [{
-//         label: "Latency",
-//         fill: true,
-//         lineTension: 0.1,
-//         backgroundColor: "rgba(75,192,192,0.4)",
-//         borderColor: "rgba(75,192,192,1)",
-//         borderCapStyle: 'butt',
-//         borderDash: [],
-//         borderDashOffset: 0.0,
-//         borderJoinStyle: 'miter',
-//         pointBorderColor: "rgba(75,192,192,1)",
-//         pointBackgroundColor: "#fff",
-//         pointBorderWidth: 1,
-//         pointHoverRadius: 5,
-//         pointHoverBackgroundColor: "rgba(75,192,192,1)",
-//         pointHoverBorderColor: "rgba(220,220,220,1)",
-//         pointHoverBorderWidth: 2,
-//         pointRadius: 1,
-//         pointHitRadius: 10,
-//         data: [65],
-//         spanGaps: false,
-//     }]
-// };
-
-// var ctx = document.getElementById("latencyChart");
-// var latencyChart = new Chart(ctx, {
-//     type: 'line',
-//     data: data,
-//     options: {
-//         legend: {
-//             display: false,
-//         },
-//         // animation:{
-//         //     duration: 2000,
-//         //     animation: 'easeInOutQuad',
-//         // },
-//         responsive: true,
-//         layout: {
-//             padding: {
-//                 left: 10,
-//                 right: 20,
-//                 top: 20
-//             },
-//         },
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     max: 500,
-//                     min: 0,
-//                     stepSize: 50
-//                 }
-//             }],
-//         }
-//     }
-// });
