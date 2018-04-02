a0367edd4ff29d09b967f1ce4bb0e04eb1c8e9e2
Tobias Wellnitz, DH1TW <tobias@dh1tw.de>
2018-04-03 01:51:12 +0200

    removed obsolete file

--- cmd/validate_topic.go
+++
@@ -1,9 +0,0 @@
-package cmd
-
-import "strings"
-
-// nats does not allow certain characters to be used as a subject (topic) name
-// validateNatsSubject will return a validated & sanitized subject string
-func validateSubject(topic string) string {
-	return strings.Replace(topic, " ", "_", -1)
-}
