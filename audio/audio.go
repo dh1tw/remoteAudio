1049012a4f72d11e82bf221fa46412e8ef1d865c
Tobias Wellnitz, DH1TW <tobias@dh1tw.de>
2019-03-08 01:09:29 +0100

    introducing Audio Node interface and added Metadata map to Audio(Msg)

--- audio/audio.go
+++ audio/audio.go
@@ -16,6 +16,14 @@ type Source interface {
 	SetCb(OnDataCb)
 }
 
+// Node is the interface for an audio node. Nodes are typically located
+// in an audio Chain between the Source and the Sink and perform custom
+// processing on (Audio)Msgs.
+type Node interface {
+	Write(Msg) error // Write data into the Node
+	SetCb(OnDataCb)  // Set the callback which will be executed when processing has finished.
+}
+
 // Sink is the interface which is implemented by an audio sink. This could
 // be an Audio player or a file for recording.
 type Sink interface {
@@ -38,8 +46,9 @@ type Token struct {
 // Msg contains an audio buffer with it's metadata.
 type Msg struct {
 	Data       []float32
-	Samplerate float64
-	Channels   int
-	Frames     int  // Number of Frames in the buffer
-	EOF        bool // End of File
+	Samplerate float64                // samplerate, e.g. 48000Hz
+	Channels   int                    // most of the time either 1 (mono) or 2 (stereo)
+	Frames     int                    // Number of Frames in the buffer
+	EOF        bool                   // End of File
+	Metadata   map[string]interface{} // storage for passing any kind of data along the audio Msg
 }
