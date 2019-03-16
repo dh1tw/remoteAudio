671ed6c0d1f10ff4cd1a58f29b66f6170da95fd9
Tobias Wellnitz, DH1TW <tobias@dh1tw.de>
2019-03-17 00:43:37 +0100

    added txUser to GUI

--- html/static/js/components/audioserver.js
+++ html/static/js/components/audioserver.js
@@ -1,13 +1,28 @@
 var AudioServer = {
     template: `
                 <div class="list-group-item">
-                    <div class="row-action-primary vcenter">
-                            <i class="fa fa-wifi" aria-hidden="true" v-bind:class="{'svr-selected': selected}" @click="setAudioServer"></i>
-                    </div>
-                    <div class="row-content">
-                      <h4 class="list-group-item-heading svr-name">{{name}}</h4>
-                      <button class="btn btn-default btn-raised" v-bind:class="{'btn-success': rxOn}" @click="setRxState"><i class="fa fa-volume-up" aria-hidden="true"></i> RX Audio</button>
-                      <p>Latency: {{latency}} ms</p>
+                    <div class="row">
+                        <div class="col-xs-3 vcenter svr-height">
+                            <div class="row-action-primary vcenter">
+                                <i class="fa fa-wifi" aria-hidden="true" v-bind:class="{'svr-selected': selected}" @click="setAudioServer"></i>
+                            </div>
+                        </div>
+                        <div class="col-xs-9">
+                            <div class="row">
+                                <h4 class="list-group-item-heading svr-name">{{name}}</h4>
+                                <button class="btn btn-default btn-raised" v-bind:class="{'btn-success': rxOn}" @click="setRxState"><i class="fa fa-volume-up" aria-hidden="true"></i> RX Audio</button>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-2">
+                                </div>
+                                <div class="col-xs-3">
+                                   <span class="label label-primary">{{latency}} ms</span>
+                                </div>
+                                <div class="col-xs-3">
+                                    <span class="label label-danger" v-bind:class="{'hidden': !txUser}">{{txUser}}</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>`,
     props: {
