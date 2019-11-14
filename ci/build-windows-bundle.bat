47d46e9fe0d0a8552cab1da1bd9057b7f2b842b0
Tobias Wellnitz, DH1TW <tobias@dh1tw.de>
2019-11-14 04:08:13 +0100

    removed static 'v' prefix in version tag of CI deployment scripts

--- ci/build-windows-bundle.bat
+++ ci/build-windows-bundle.bat
@@ -13,7 +13,7 @@ REM copy the needed shared libraries and the binary
 %MSYS_PATH%\usr\bin\bash -lc "cp /c/gopath/src/github.com/dh1tw/remoteAudio/remoteAudio.exe /c/gopath/src/github.com/dh1tw/remoteAudio/release"
 
 REM zip everything
-%MSYS_PATH%\usr\bin\bash -lc "cd /c/gopath/src/github.com/dh1tw/remoteAudio/release && 7z a -tzip remoteAudio-v$APPVEYOR_REPO_TAG_NAME-$GOOS-$GOARCH.zip *"
+%MSYS_PATH%\usr\bin\bash -lc "cd /c/gopath/src/github.com/dh1tw/remoteAudio/release && 7z a -tzip remoteAudio-$APPVEYOR_REPO_TAG_NAME-$GOOS-$GOARCH.zip *"
 
 REM copy it into the build folder
 copy %GOPATH%\src\github.com\dh1tw\remoteAudio\release\remoteAudio-v%APPVEYOR_REPO_TAG_NAME%-%GOOS%-%GOARCH%.zip %APPVEYOR_BUILD_FOLDER_ORIGINAL%
